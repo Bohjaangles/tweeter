f12fa26b43a475a8a5c8a6d4747ae691d41839d6
Russel Mueller <bohjaangles@gmail.com>
2022-11-10 02:31:47 +0000

    introduce new JS functions into client.js file, including createTweetElement, renderTweets and loadTweets. this changes tweets that are displayed to be served from /tweets instead of being hardcoded into client.js. Adding a timeago library so that when a tweet was made is accurately displayed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,55 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+const createTweetElement = function(tweetObject) {
+  const $tweet = $(`
+  <article class="tweet">
+    <header>
+      <div><i class="fa-solid fa-user"></i><h3>${tweetObject.user.name}</h3></div><h4 id="usertag">${tweetObject.user.handle}</h4>
+    </header>
+  <p>${tweetObject.content.text}</p>
+    <footer>
+      <h6 class="footerAlign">${timeago.format(tweetObject.created_at)}</h6><div class="footerAlign"><i class="fa-solid fa-flag fa-2xs"></i><i class="fa-solid fa-retweet fa-s"></i><i class="fa-solid fa-heart fa-2xs">  </i></div>
+    </footer>
+  </article>`);
+  return $tweet;
+};
+
+const renderTweets = function($tweets) {
+  // loops through tweets
+  for (const $tweet of $tweets) {
+      $('#tweets-container').append(createTweetElement($tweet));
+    };
+};
+
+const loadTweets = () => {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    dataType: 'json',
+    success: (data) => {
+      renderTweets(data);
+    },
+    error: (error) => {
+      console.log('this request failed and this was the error', error);
+    }
+  });
+};
+
+$(document).ready(function() {
+  $('.newTweet').submit(function(event) {
+    event.preventDefault();
+    console.log($(this).serialize());
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+    })
+    .then(console.log('hey'));
+  })
+});
+
+$(document).ready(() => {
+  loadTweets();
+});
